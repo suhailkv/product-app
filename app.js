bf81bfbb902e3ecf558b56ac0242514ad371265e
Suhail k <suhailudheen.m@spectrum7tech.com>
2024-08-23 16:57:49 +0530

    initial commit , server created

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors'); 
+const helmet = require('helmet'); 
+const compression = require('compression');
+
+const routes = require('./routes/index'); 
+
+const env = process.env.NODE_ENV || 'development';
+require('dotenv').config({ path: `.env.${env}` });
+
+const app = express();
+const port = process.env.PORT || 3000;
+app.set('port',port)
+
+app.use(cors());
+app.use(helmet()); 
+app.use(compression());
+app.use(express.json());
+
+app.use('/', routes);
+
+module.exports = app;
