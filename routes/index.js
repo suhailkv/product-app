dfbcda93eee8ee1a01bd992f15264a0c86398ec6
Suhail k <suhailudheen.m@spectrum7tech.com>
2024-08-24 00:07:15 +0530

    added and configured constants

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const productController = require('../controllers/productController');
-
+const CONSTANTS = require('../constants')
 const router = express.Router();
 
-router.post('/products', productController.createProduct);
-router.get('/products', productController.getAllProducts);
-router.get('/products/:product_id', productController.getProductWithProfit);
+router.post(`/${CONSTANTS.PRODUCT_API}`, productController.createProduct);
+router.get(`/${CONSTANTS.PRODUCT_API}`, productController.getAllProducts);
+router.get(`/${CONSTANTS.PRODUCT_API}/:product_id`, productController.getProductWithProfit);
 
 
 module.exports = router;
